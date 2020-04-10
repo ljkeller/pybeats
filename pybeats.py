dbb90b047f968bd6210ffe9f5373e8e52e32cdda
ljkeller <kellerjlucas@gmail.com>
2020-04-10 11:51:56 -0500

    Functioning visualizer, has some problems with keeping up to date on certain files. Probably just because python threading is trash

--- pybeats.py
+++ pybeats.py
@@ -33,7 +33,7 @@ def draw_figure(canvas, figure, loc=(0,0)):
 # plot with various axes scales
 plt.figure(1)
 plt.axis('off')
-plt.margins(0.1)
+plt.margins(0.01)
 plt.tight_layout()
 plt.ion()
 fig = plt.gcf()
@@ -44,12 +44,19 @@ ls.sort()
 ls = list(filter(lambda f: f.endswith(".wav"), ls))
 
 # All this inside window
-layout = [    [sg.Text("Lets get started making beats", font="Any 24", text_color=PRI_PURP)],
-              [sg.Listbox(ls, size=(30, 40), key='-File Path-', background_color=PRI_PURP), 
+layout = [    [sg.Text("PyBeats", font="Any 24", text_color=PRI_PURP)],
+              [sg.Listbox(ls, size=(30, 26), font="Any 14",
+                  key='-File Path-', background_color=PRI_PURP), 
                   sg.Canvas(size=(figure_w, figure_h), key='-CANVAS-')],
-              [sg.Text("What file do you want to modify?")],
-              [sg.Input(size=(45,1), background_color="white"), sg.FileBrowse(file_types=(("ALL Files", "*.wav"),))],
-              [sg.Button('Play'), sg.Button('Cancel')]]
+              [sg.Text("What file do you want to modify?", font="Any 14")],
+              [sg.Input(size=(45,1), background_color="white", text_color=PRI_PURP,
+                  font="Any 14"), 
+                  sg.FileBrowse(file_types=(("ALL Files", "*.wav"),), font="Any 14"),
+                  sg.Button('Play', button_color=("white", PRI_PURP), font="Any 14"), 
+                  sg.Button('Base Boost', button_color=("white", PRI_PURP), font="Any 14"),
+                  sg.Button('Mid Boost', button_color=("white", PRI_PURP), font="Any 14"),
+                  sg.Button('Cancel', font="Any 14")]   
+        ]
 
 # Create the window
 win_name = "Py Beats (" + os.getcwd() + ")"
@@ -73,7 +80,9 @@ while True:
             signal = wave.open(fpath, mode='rb')
             fs = signal.getframerate()
             signal = np.frombuffer(signal.readframes(-1), dtype='int16')
+            n_samples = signal.size
             n_signal = signal.size
+            print(n_signal * 1./fs)
             
             # Identify time axis and number of play loops
             t = np.linspace(0, n_signal / fs, num=n_signal)
@@ -85,13 +94,14 @@ while True:
             n_omegas = omegas.size
 
             max_mag = max(max(fourier_coeffs.real), max(fourier_coeffs.imag))
-            max_w = useful_freqs(omegas, fourier_coeffs, max_mag/10)
 
             audio_thread.start()
+            audio_tid = audio_thread.native_id
+            start_time = time.time()
             for i in range(0, n_loops):
                 # Break signal up into chunks for live analysis
-                start = int(i * n_omegas/n_loops)
-                end = int((i + 1) * n_omegas/n_loops)
+                start = int(i * n_samples//n_loops)
+                end = int((i + 1) * n_samples//n_loops)
 
                 #re-establish fouier coeffs
                 n_signal = len(signal[start:end])
@@ -101,13 +111,17 @@ while True:
                 #live plotting
                 fig.clear()
                 plt.ylim(-max_mag/10, max_mag/10)
-                plt.xlim(0, AUD_FREQ)
-                plt.axis('off')
+                plt.xscale('log')
+                #plt.xticks([1, 10, 100, 1000, 40000, 100000])
+                #plt.axis('off')
                 plt.margins(0.001)
                 plt.plot(omegas, fourier_coeffs.real, PRI_PURP, omegas, fourier_coeffs.imag,
                         PRI_PURP)
                 fig_visualizer.draw()
-                event, values = window.read(timeout=(1./fs) * n_signal * 1000)
+                playtime_ms = ((1./fs) * n_signal) * 1000
+                elapsed_time_ms = (time.time() - start_time) * 1000 
+                event, values = window.read(timeout=playtime_ms - elapsed_time_ms)
+                start_time = time.time()
             
             audio_thread.join()
         else:
